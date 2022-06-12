28ceef0cd005455c4b0be6193040ac7e2b2b4302
binotation <68807919+binotation@users.noreply.github.com>
2022-06-12 14:05:55 +1000

    Renamed parse_args to get_args

--- src/clientlib.c
+++ src/clientlib.c
@@ -1,10 +1,10 @@
 #include "clientlib.h"
 
-enum ParseArgsResult parse_args( Args *args, int argc, char **argv )
+enum GetArgsResult get_args( Args *args, int argc, char **argv )
 {
     FILE *authfile;
-    if( argc != 4 && argc != 5 ) return PARSE_ARGS_INVALID_ARGS_COUNT;
-    else if( !( authfile = fopen( argv[2], "r" ) )) return PARSE_ARGS_AUTHFILE_NOT_FOUND;
+    if( argc != 4 && argc != 5 ) return GET_ARGS_INVALID_ARGS_COUNT;
+    else if( !( authfile = fopen( argv[2], "r" ) )) return GET_ARGS_AUTHFILE_NOT_FOUND;
 
     args->chosen_name = argv[1];
     dynstring_readline( &args->authdstr, authfile );
@@ -21,5 +21,5 @@ enum ParseArgsResult parse_args( Args *args, int argc, char **argv )
     }
 
     fclose(authfile);
-    return PARSE_ARGS_SUCCESS;
+    return GET_ARGS_SUCCESS;
 }
--- src/clientlib.h
+++ src/clientlib.h
@@ -5,7 +5,7 @@
 
 #define USAGE "Usage: client name authfile [host] port\n"
 
-// Client return values
+// Client exit codes
 #define NO_ERR 0;
 #define ARGS_ERR 1;
 #define COMM_ERR 2;
@@ -19,7 +19,7 @@ typedef struct
     int num;
 } AcceptedName;
 
-// Parsed args.
+// User args
 typedef struct
 {
     char *chosen_name;
@@ -35,18 +35,18 @@ typedef struct
     FILE *write;
 } ServerStreams;
 
-enum ParseArgsResult
+enum GetArgsResult
 {
-    PARSE_ARGS_SUCCESS,
-    PARSE_ARGS_AUTHFILE_NOT_FOUND,
-    PARSE_ARGS_INVALID_ARGS_COUNT,
+    GET_ARGS_SUCCESS,
+    GET_ARGS_AUTHFILE_NOT_FOUND,
+    GET_ARGS_INVALID_ARGS_COUNT,
 };
 
 /**
- * Parse argv into args.
+ * Get args from argv.
  * @param args	Args struct, authdstr must be initialized.
  * @returns if successful, authfile not found or invalid args count
  */
-enum ParseArgsResult parse_args( Args *args, int argc, char **argv );
+enum GetArgsResult get_args( Args *args, int argc, char **argv );
 
 #endif
